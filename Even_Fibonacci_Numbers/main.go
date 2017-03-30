f608a8b7f183b1b0a08f0fc78767d0c05aa0d902
booleanalgebra <davidgodin@David-Godins-MacBook-Pro-3.local>
2017-03-30 02:01:30 -0700

    Format Change

--- Even_Fibonacci_Numbers/main.go
+++ Even_Fibonacci_Numbers/main.go
@@ -2,14 +2,16 @@ package main
 
 import "fmt"
 
-func main (){
-	var x[50] int32 //also slice, x:= make([] int, 50)
+func main() {
+	var x [50]int32 //also slice, x:= make([] int, 50)
 	var fib_sum int32
 	x[1] = 1 //initialize these values so there is something to add
 	x[2] = 2
 	fmt.Println("All even Fibonacci numbers < 4,000,000:")
-	for i := 2; i<50; i++ {x[i] = (x[i-1] + x[i-2])
-		if (x[i]<4000000)&&(x[i] % 2 == 0)&&(x[i]>0){fib_sum += x[i]
+	for i := 2; i < 50; i++ {
+		x[i] = (x[i-1] + x[i-2])
+		if (x[i] < 4000000) && (x[i]%2 == 0) && (x[i] > 0) {
+			fib_sum += x[i]
 			fmt.Println(x[i])
 		}
 	}
